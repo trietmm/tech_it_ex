9fba1a20d7f926666abd96711fefcdebeabc196f
MinhTriet <mtv.mmtriet@gmail.com>
2017-11-19 23:40:43 +0700

    interface candidate

---
+++ mti_tech_interview_examination/Lib/Interface/ICandidate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using mti_tech_interview_examination.Models.Entity;
+
+namespace mti_tech_interview_examination.Lib.Interface
+{
+    interface ICandidate
+    {
+        void Register(Mti_Candidate candidate);
+    }
+}
